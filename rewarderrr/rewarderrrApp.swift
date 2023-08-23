16d76d2d44bfbdf11a4dc23223adcd6532a73a56
Zoe Gorman <zoe@zoewilla.com>
2023-08-23 16:31:11 -0400

    Initial Commit

---
+++ rewarderrr/rewarderrrApp.swift
@@ -0,0 +1,17 @@
+//
+//  rewarderrrApp.swift
+//  rewarderrr
+//
+//  Created by Zoe Gorman on 8/23/23.
+//
+
+import SwiftUI
+
+@main
+struct rewarderrrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
